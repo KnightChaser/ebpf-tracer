5500ec0cf5546a4f9c21ccf75ddb641e54829e80
knightchaser <agerio100@naver.com>
2025-07-18 22:12:52 +0900

    chore: apply .clang-format to set 1 indentation = 4 width

--- test/test.c
+++ test/test.c
@@ -5,7 +5,7 @@
 
 int main(int argc __attribute_maybe_unused__,
          char *argv[] __attribute_maybe_unused__) {
-  puts("Hello from the test program!");
+    puts("Hello from the test program!");
 
-  return 0;
+    return 0;
 }
