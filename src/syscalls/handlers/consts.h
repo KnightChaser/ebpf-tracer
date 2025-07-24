30fa38c5b372954809490411ade2e2882620b236
KnightChaser <agerio100@naver.com>
2025-07-24 15:11:19 +0900

    chore: deduplicate open_flags definition into a common header file

---
+++ src/syscalls/handlers/consts.h
@@ -0,0 +1,23 @@
+// src/syscalls/handlers/consts.h
+#ifndef CONSTS_H
+#define CONSTS_H
+
+#include "../utils.h"
+#include <fcntl.h>
+
+/**
+ * Converts flags to a string representation.
+ * This function takes a set of flags and converts them into a human-readable
+ * string format, using a predefined array of flag names.
+ */
+static const struct flag_name open_flags[] = {
+    {O_CREAT, "O_CREAT"},       {O_EXCL, "O_EXCL"},
+    {O_NOCTTY, "O_NOCTTY"},     {O_TRUNC, "O_TRUNC"},
+    {O_APPEND, "O_APPEND"},     {O_NONBLOCK, "O_NONBLOCK"},
+    {O_DSYNC, "O_DSYNC"},       {O_SYNC, "O_SYNC"},
+    {O_RSYNC, "O_RSYNC"},       {O_DIRECTORY, "O_DIRECTORY"},
+    {O_NOFOLLOW, "O_NOFOLLOW"}, {O_CLOEXEC, "O_CLOEXEC"},
+    // NOTE: may be added if needed >_<
+};
+
+#endif // CONSTS_H
--- src/syscalls/handlers/handle_open.c
+++ src/syscalls/handlers/handle_open.c
@@ -1,22 +1,8 @@
 // src/syscalls/handlers/handle_open.c
 #define _GNU_SOURCE
 #include "../open_common.h"
+#include "consts.h"
 #include <fcntl.h>
-
-/**
- * Converts flags to a string representation.
- * This function takes a set of flags and converts them into a human-readable
- * string format, using a predefined array of flag names.
- */
-static const struct flag_name open_flags[] = {
-    {O_CREAT, "O_CREAT"},       {O_EXCL, "O_EXCL"},
-    {O_NOCTTY, "O_NOCTTY"},     {O_TRUNC, "O_TRUNC"},
-    {O_APPEND, "O_APPEND"},     {O_NONBLOCK, "O_NONBLOCK"},
-    {O_DSYNC, "O_DSYNC"},       {O_SYNC, "O_SYNC"},
-    {O_RSYNC, "O_RSYNC"},       {O_DIRECTORY, "O_DIRECTORY"},
-    {O_NOFOLLOW, "O_NOFOLLOW"}, {O_CLOEXEC, "O_CLOEXEC"},
-    // NOTE: may be added if needed >_<
-};
 
 /**
  * Handles the entry of the open syscall.
