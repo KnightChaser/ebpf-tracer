88947a61dfcef98cbdb10bbb022491e202affff9
KnightChaser <agerio100@naver.com>
2025-07-28 17:44:51 +0900

    refactor: refactor read ssycall handlers with logger functions

--- src/syscalls/handlers/handle_read.c
+++ src/syscalls/handlers/handle_read.c
@@ -1,6 +1,7 @@
 // src/syscalls/handlers/handle_read.c
 #define _GNU_SOURCE
 #include "../../syscalls/syscalls.h"
+#include "../../utils/logger.h"
 #include "../fd_cache.h"
 #include "../read_common.h"
 #include <stdio.h>
@@ -16,25 +17,24 @@
 void handle_read_enter(pid_t pid __attribute__((unused)),
                        const struct syscall_event *e) {
     struct read_args args;
-    if (fetch_read_args(pid, e, &args) == 0) {
-
-        printf("%-6ld %-16s(%d, %p, %zu)\n",
-               e->syscall_nr,    // syscall number
-               e->enter.name,    // syscall name
-               args.fd,          // file descriptor
-               (void *)args.buf, // buffer pointer
-               args.count        // number of bytes to read
-        );
-
-        const char *path = fd_cache_get(args.fd);
-        if (path) {
-            printf(" => path: %s\n", path);
-        }
-    } else {
-        // fallback to the default printer
+    if (fetch_read_args(pid, e, &args) != 0) {
         handle_sys_enter_default(pid, e);
+        return;
     }
-    fflush(stdout);
+
+    char argbuf[64];
+    snprintf(argbuf, sizeof(argbuf), "%d, %p, %zu",
+             args.fd,          // file descriptor
+             (void *)args.buf, // buffer pointer
+             args.count);      // count of bytes to read
+    log_syscall(e->syscall_nr, e->enter.name, argbuf, /*retval=*/0);
+
+    const char *path = fd_cache_get(args.fd);
+    if (path) {
+        log_kv("path", "%s", path);
+    } else {
+        log_kv("path", "<unknown>");
+    }
 }
 
 /**
@@ -47,5 +47,5 @@ void handle_read_enter(pid_t pid __attribute__((unused)),
 void handle_read_exit(pid_t pid __attribute__((unused)),
                       const struct syscall_event *e) {
     long ret = e->exit.retval;
-    printf(" = %ld (read)\n", ret);
+    log_kv("ret", "%ld (read)", ret);
 }
